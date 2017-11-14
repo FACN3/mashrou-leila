99fc064b179d5ffe58fd580d0acaba458cf0ed1a
Amir Khoury <amirkhourykh@gmail.com>
2017-11-14 12:57:18 +0200

    added AddEventListener to the logic.js and added ID's to the html file. Reference #3.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,31 @@
 (function() {
-  var xhr = new XMLHttpRequest();
-  var guardian_key = '&api-key=5ece3fcc-ecb6-44cf-a6b5-ba723185825e'
-  var search_request = 'nazareth';
-  var url = 'https://content.guardianapis.com/search?q=' + search_request + guardian_key;
+    var xhr = new XMLHttpRequest();
+    var guardian_key = '&api-key=5ece3fcc-ecb6-44cf-a6b5-ba723185825e'
+    var url = 'https://content.guardianapis.com/search?q=' + search_request + guardian_key;
+    var search_request = ' ';
 
-  xhr.onreadystate = function () {
-    if (readyState == 4 && xhr.status == 200) {
-      var guardian_obj = JSON.parase(xhr.responseText);
-      //var image = 
-      //var link = gif_obj.response.results[1]+guardian_key;
-    }
+
+
+    console.log(document.getElementById('button'));
+    document.getElementById('button').addEventListener('submit', function(event) {
+      var search_request = document.getElementById('search').value;
+
+
+      xhr.onreadystate = function() {
+        if (readyState == 4 && xhr.status == 200) {
+          var guardian_obj = JSON.parase(xhr.responseText);
+          //var image =
+          var link = gif_obj.response.results[1].webUrl + guardian_key;
+          console.log(link)
+        }
+      }
+      xhr.open('GET', url, true);
+      xhr.send();
+    });
+
+
+    console.log(xhr);
+
   }
-console.log(xhr);
-
-}
 
 )();
