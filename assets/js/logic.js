c47cfe9c145fe4e617738b60795778c6b84196db
Amir Khoury <amirkhourykh@gmail.com>
2017-11-14 14:44:38 +0200

    Fixed the button functionalitygit add .! (Yay...). Reference

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,30 +1,29 @@
 (function() {
     var xhr = new XMLHttpRequest();
-    var guardian_key = '&api-key=5ece3fcc-ecb6-44cf-a6b5-ba723185825e'
-    var url = 'https://content.guardianapis.com/search?q=' + search_request + guardian_key;
-    var search_request = ' ';
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        console.log('hello amir');
+        var guardian_obj = JSON.parse(xhr.response);
+        console.log(guardian_obj);
+        var link = guardian_obj.response.results[1].webUrl ;
+        console.log(link)
+      }}
+
+      var guardian_key = '&api-key=5ece3fcc-ecb6-44cf-a6b5-ba723185825e'
 
 
-
-    document.getElementById('button').addEventListener('button', function() {
-      var search_request = document.getElementById('search').value;
-      console.log('you clicked the button');
-
-      xhr.onreadystate = function() {
-        if (readyState == 4 && xhr.status == 200) {
-          console.log('hello amir');
-          var guardian_obj = JSON.parase(xhr.responseText);
-          console.log(guardian_obj);
-          var link = gif_obj.response.results[1].webUrl + guardian_key;
-          console.log(link)
-        }
-      }
-      xhr.open('GET', url, true);
-      xhr.send();
-    });
+      document.getElementById('todo_form').addEventListener('submit', function(event) {
+          event.preventDefault();
+          var search_request = document.getElementById('search').value;
+          console.log('you clicked the button');
+          var url = 'https://content.guardianapis.com/search?q=' + search_request + guardian_key;
+          console.log(url);
+          xhr.open('GET', url, true);
+           xhr.send();
+      });
 
 
-    console.log('xhr',xhr);
 
   }
 
