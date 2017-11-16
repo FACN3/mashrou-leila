d4dffc571eb9c490e9a99579445ae7fe0af0e49b
Amir Khoury <amirkhourykh@gmail.com>
2017-11-16 11:47:17 +0200

    Removed the console.logs the comments and the uncessary spaces. Referenced

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -3,8 +3,6 @@
   var guardian_key = 'api-key=5ece3fcc-ecb6-44cf-a6b5-ba723185825e';
   var guardian_api = 'https://content.guardianapis.com/search?q=';
   var guardian_fields = '&show-fields=thumbnail,headline,shortUrl&';
-
-  //
   var twitter_con = document.getElementById("twitter_con");
   var wiki_con = document.getElementById("wikipedia_con");
   var guardian_con = document.getElementById("guardian_con");
@@ -20,38 +18,32 @@
     req.send();
   };
 
-
   document.getElementById('todo_form').addEventListener('submit', function(event) {
 
     event.preventDefault();
 
-    twitter_con.innerHTML="";
-    wiki_con.innerHTML="";
-    guardian_con.innerHTML="";
+    twitter_con.innerHTML = "";
+    wiki_con.innerHTML = "";
+    guardian_con.innerHTML = "";
     var search_request = document.getElementById('search').value;
     var twitter_url = "https://welvon-1211.appspot.com/hello?query=" + search_request;
     var guardian_url = guardian_api + search_request + guardian_fields + guardian_key;
     var wikipedia_url = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=" + search_request +
       "&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=2&exlimit=max&origin=*&pithumbsize=500";
 
-    fetch('GET',twitter_url,filter_twitter,render_twitter);
+    fetch('GET', twitter_url, filter_twitter, render_twitter);
     fetch('GET', guardian_url, filter_guardian, render_guardian);
     fetch('GET', wikipedia_url, filter_wikipedia, render_wikipedia);
 
   });
 
   function render_guardian(array) {
-    //  console.log(array);
-    //console.log(guardian_con);
-   image_guardian=document.createElement('img');
-   image_guardian.src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/The_Guardian_2.svg/2000px-The_Guardian_2.svg.png";
-   guardian_con.appendChild(image_guardian);
 
+    image_guardian = document.createElement('img');
+    image_guardian.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/The_Guardian_2.svg/2000px-The_Guardian_2.svg.png";
+    guardian_con.appendChild(image_guardian);
     array.forEach(function(obj) {
-      //  console.log(Object.keys(obj));
-      //console.log(Object.values(obj));
       var link = document.createElement('a');
-      //  console.log('link', link);
       link.href = obj['shortUrl'];
       var image = document.createElement('img');
       image.src = obj['thumbnail'];
@@ -62,61 +54,43 @@
       guardian_con.appendChild(title);
 
       guardian_con.appendChild(link);
-      //console.log(guardian_con);
     });
   }
 
   function render_twitter(html_text) {
-var image_twitter =document.createElement('img');
- image_twitter.src="http://clipart-library.com/image_gallery2/Twitter-PNG-HD.png";
-//
-   //twitter_con.appendChild(image_twiiter);
-  //var  div1=document.createElement('div')
+    var image_twitter = document.createElement('img');
+    image_twitter.src = "http://clipart-library.com/image_gallery2/Twitter-PNG-HD.png";
 
-    //dvi1.innerHTML = s;
-    twitter_con.innerHTML=s;
+    twitter_con.innerHTML = s;
     twttr.widgets.load();
-
-
   };
 
-
-
   function render_wikipedia(data) {
-var  image_wikipedia=document.createElement('img');
-    image_wikipedia.src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Wikipedia-logo-v2-simple.svg/250px-Wikipedia-logo-v2-simple.svg.png";
-
-    //   image_wikipedia.style.width="500px";
-      // image_wikipedia.style.height="400px";
-
+    var image_wikipedia = document.createElement('img');
+    image_wikipedia.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Wikipedia-logo-v2-simple.svg/250px-Wikipedia-logo-v2-simple.svg.png";
 
     wiki_con.appendChild(image_wikipedia);
 
     data.forEach(function(obj) {
-        var link= document.createElement('a')
-        link.href=obj.url;
+      var link = document.createElement('a')
+      link.href = obj.url;
       var wikicontent = document.createElement('span');
       wikicontent.textContent = obj.content;
       var wikititle = document.createElement('span');
       wikititle.textContent = obj.title;
 
+      if (obj.image) {
 
-      //var wikiphoto = document.createElement('img');
+        image = document.createElement('img');
+        image.src = obj.image;
+        wikititle.appendChild(image);
+      }
 
-        if (obj.image) {
-
-            image=document.createElement('img');
-            image.src=obj.image;
-            wikititle.appendChild(image);
-        }
-
-       wikititle.appendChild(wikicontent);
-       link.appendChild(wikititle);
+      wikititle.appendChild(wikicontent);
+      link.appendChild(wikititle);
 
       wiki_con.appendChild(link);
     });
-
-
   }
 
   window.twttr = (function(d, s, id) {
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,14 +5,11 @@ function filter_guardian(data) {
   data.response['results'].forEach(function(task) {
     guardian_array.push(task.fields);
   })
-
-  //console.log(typeof(guardian_array));
   return guardian_array;
 
 }
-//parse twiter
+
 function filter_twitter(data) {
-  //var twitter_con = document.getElementById("twitter_con");
   arr_result = data.href;
   var ids = [];
   for (var i = 0; i < arr_result.length; i += 1) {
@@ -26,30 +23,23 @@ function filter_twitter(data) {
   return s;
 }
 
-
 function filter_wikipedia(data) {
   object = data.query.pages;
 
-     var  arr = [];
+  var arr = [];
 
   Object.keys(object).forEach(function(key) {
 
-      var extract = {
-        'title': object[key].title,
-        'content': object[key].extract,
-          'url':"http://en.wikipedia.org/wiki?curid="+object[key].pageid 
-          };
+    var extract = {
+      'title': object[key].title,
+      'content': object[key].extract,
+      'url': "http://en.wikipedia.org/wiki?curid=" + object[key].pageid
+    };
 
-        if(object[key].thumbnail){
-          extract.image=object[key].thumbnail.source;
-        }
-        //console.log(object[key].extract);
-        arr.push(extract);
-    });
-
-     return(arr);
-
-
-
-
+    if (object[key].thumbnail) {
+      extract.image = object[key].thumbnail.source;
+    }
+    arr.push(extract);
+  });
+  return (arr);
 }
